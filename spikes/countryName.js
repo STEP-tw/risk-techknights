e2605af1c5992bfb5a139a564cc854327e99a040
Arif <44020819+azimarif@users.noreply.github.com>
2019-02-16 13:06:43 +0530

    Update Map UI

--- spikes/countryName.js
+++ spikes/countryName.js
@@ -1,43 +1,69 @@
-territoriesCoordinates = {};
-territoriesCoordinates.afghanistan = { x: 675, y: 310 };
-territoriesCoordinates.alaska = { x: 226, y: 200 };
-territoriesCoordinates.alberta = { x: 285, y: 242 };
-territoriesCoordinates.argentina = { x: 362, y: 533 };
-territoriesCoordinates.brazil = { x: 400, y: 450 };
-territoriesCoordinates.central_america = { x: 294, y: 359 };
-territoriesCoordinates.china = { x: 755, y: 345 };
-territoriesCoordinates.congo = { x: 572, y: 491 };
-territoriesCoordinates.east_africa = { x: 615, y: 470 };
-territoriesCoordinates.eastern_australia = { x: 864, y: 530 };
-territoriesCoordinates.eastern_united_states = { x: 347, y: 315 };
-territoriesCoordinates.egypt = { x: 573, y: 403 };
-territoriesCoordinates.great_britain = { x: 475, y: 278 };
-territoriesCoordinates.greenland = { x: 430, y: 175 };
-territoriesCoordinates.iceland = { x: 490, y: 223 };
-territoriesCoordinates.india = { x: 717, y: 385 };
-territoriesCoordinates.indonesia = { x: 779, y: 483 };
-territoriesCoordinates.irkutsk = { x: 770, y: 250 };
-territoriesCoordinates.japan = { x: 850, y: 300 };
-territoriesCoordinates.kamchatka = { x: 836, y: 195 };
-territoriesCoordinates.madagascar = { x: 644, y: 554 };
-territoriesCoordinates.middle_east = { x: 629, y: 378 };
-territoriesCoordinates.mongolia = { x: 779, y: 296 };
-territoriesCoordinates.new_guinea = { x: 844, y: 465 };
-territoriesCoordinates.north_africa = { x: 520, y: 430 };
-territoriesCoordinates.northern_europe = { x: 545, y: 289 };
-territoriesCoordinates.northwest_territory = { x: 300, y: 198 };
-territoriesCoordinates.ontario = { x: 338, y: 245 };
-territoriesCoordinates.peru = { x: 354, y: 458 };
-territoriesCoordinates.quebec = { x: 387, y: 249 };
-territoriesCoordinates.scandinavia = { x: 552, y: 222 };
-territoriesCoordinates.siam = { x: 775, y: 402 };
-territoriesCoordinates.siberia = { x: 722, y: 215 };
-territoriesCoordinates.south_africa = { x: 580, y: 552 };
-territoriesCoordinates.southern_europe = { x: 548, y: 333 };
-territoriesCoordinates.ukraine = { x: 614, y: 262 };
-territoriesCoordinates.ural = { x: 682, y: 232 };
-territoriesCoordinates.venezuela = { x: 353, y: 396 };
-territoriesCoordinates.western_australia = { x: 823, y: 558 };
-territoriesCoordinates.western_europe = { x: 493, y: 345 };
-territoriesCoordinates.western_united_states = { x: 291, y: 298 };
-territoriesCoordinates.yakursk = { x: 777, y: 189 };
+territoriesCoordinates = {
+  afghanistan: { x: 675, y: 310 },
+  alaska: { x: 226, y: 200 },
+  alberta: { x: 285, y: 242 },
+  argentina: { x: 362, y: 533 },
+  brazil: { x: 400, y: 450 },
+  central_america: { x: 294, y: 359 },
+  china: { x: 755, y: 345 },
+  congo: { x: 572, y: 491 },
+  east_africa: { x: 615, y: 470 },
+  eastern_australia: { x: 864, y: 530 },
+  eastern_united_states: { x: 347, y: 315 },
+  egypt: { x: 573, y: 403 },
+  great_britain: { x: 475, y: 278 },
+  greenland: { x: 430, y: 175 },
+  iceland: { x: 490, y: 220 },
+  india: { x: 717, y: 385 },
+  indonesia: { x: 779, y: 483 },
+  irkutsk: { x: 770, y: 250 },
+  japan: { x: 850, y: 300 },
+  kamchatka: { x: 836, y: 195 },
+  madagascar: { x: 644, y: 556 },
+  middle_east: { x: 629, y: 378 },
+  mongolia: { x: 779, y: 296 },
+  new_guinea: { x: 844, y: 465 },
+  north_africa: { x: 520, y: 430 },
+  northern_europe: { x: 545, y: 289 },
+  northwest_territory: { x: 300, y: 198 },
+  ontario: { x: 338, y: 245 },
+  peru: { x: 354, y: 458 },
+  quebec: { x: 387, y: 249 },
+  scandinavia: { x: 552, y: 222 },
+  siam: { x: 775, y: 402 },
+  siberia: { x: 722, y: 215 },
+  south_africa: { x: 580, y: 552 },
+  southern_europe: { x: 548, y: 333 },
+  ukraine: { x: 614, y: 262 },
+  ural: { x: 682, y: 232 },
+  venezuela: { x: 353, y: 396 },
+  western_australia: { x: 823, y: 558 },
+  western_europe: { x: 493, y: 345 },
+  western_united_states: { x: 291, y: 298 },
+  yakursk: { x: 777, y: 189 }
+};
+
+seaLine = {
+  alaska: { x1: "100", y1: "205", x2: "200", y2: "205" },
+  kamchatka: { x1: "860", y1: "205", x2: "930", y2: "205" },
+  northwest_territory: { x1: "360", y1: "190", x2: "400", y2: "180" },
+  ontario: { x1: "333", y1: "225", x2: "400", y2: "180" },
+  quebec: { x1: "380", y1: "215", x2: "400", y2: "180" },
+  greenland: { x1: "448", y1: "200", x2: "462", y2: "215" },
+  iceland: { x1: "500", y1: "235", x2: "525", y2: "250" },
+  scandinavia: { x1: "482", y1: "255", x2: "525", y2: "250" },
+  great_britain: { x1: "500", y1: "235", x2: "482", y2: "255" },
+  northern_europe: { x1: "482", y1: "255", x2: "525", y2: "280" },
+  western_europe: { x1: "482", y1: "300", x2: "490", y2: "320" },
+  egypt: { x1: "565", y1: "370", x2: "565", y2: "390" },
+  north_africa: { x1: "450", y1: "440", x2: "475", y2: "430" },
+  middle_east: { x1: "620", y1: "460", x2: "635", y2: "430" },
+  east_africa: { x1: "625", y1: "490", x2: "632", y2: "545" },
+  madagascar: { x1: "610", y1: "555", x2: "632", y2: "545" },
+  japan: { x1: "820", y1: "265", x2: "845", y2: "288" },
+  mongolia: { x1: "810", y1: "300", x2: "845", y2: "288" },
+  siam: { x1: "790", y1: "435", x2: "800", y2: "456" },
+  new_guinea: { x1: "800", y1: "456", x2: "818", y2: "462" },
+  indonesia: { x1: "818", y1: "462", x2: "840", y2: "502" },
+  western_australia: { x1: "810", y1: "502", x2: "820", y2: "520" }
+}
--- spikes/script.js
+++ spikes/script.js
@@ -1,43 +1,66 @@
-let svg = document.getElementById("mySvg");
-let worldDiv = document.createElement("div");
+let svg = document.getElementById('mySvg');
+let worldDiv = document.createElement('div');
 for (let territoryPath in territoriesPath) {
   let pathTag = `<a id="${territoryPath}" onclick="changeColor()"> 
-	<path d="${territoriesPath[territoryPath]}"></path></a>`;
+    <path d="${territoriesPath[territoryPath]}"></path></a>`;
   worldDiv.append(pathTag);
 }
-svg.innerHTML = "<text x=500 y=150>Prince</text>";
-svg.innerHTML += worldDiv.innerText;
+a = worldDiv.innerText;
 
-const changeColor = function() {
-  console.log("called");
-};
 
-const addText = function(path) {
-  let territoryName = path.parentElement.id;
-  let name = document.createElementNS("http://www.w3.org/2000/svg", "text");
-  let xCoordinate = territoriesCoordinates[territoryName].x;
-  let yCoordinate = territoriesCoordinates[territoryName].y;
-  name.setAttribute("transform", `translate(${xCoordinate} ${yCoordinate})`);
-  name.style.fill = "green";
-  name.style.textAnchor = "middle";
-  name.textContent = path.parentElement.id.replace(/_/g, " ");
-  name.setAttribute("font-size", "8");
-  path.parentNode.insertBefore(name, path.nextSibling);
-  yCoordinate = yCoordinate + 10;
+let lineDivs = document.createElement('div');
+Object.keys(seaLine).forEach(line => {
+  let line1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+  line1.id = line;
+  line1.setAttributeNS("http://www.w3.org/2000/svg", "x1", seaLine[line].x1);
+  line1.setAttributeNS("http://www.w3.org/2000/svg", "x2", seaLine[line].x2);
+  line1.setAttributeNS("http://www.w3.org/2000/svg", "y1", seaLine[line].y1);
+  line1.setAttributeNS("http://www.w3.org/2000/svg", "y2", seaLine[line].y2);
+  lineDivs.append(line1);
+});
+svg.innerHTML = lineDivs.innerHTML + a;
+
+i=1;
+const changeColor = function () {
+  let x = +event.target.parentElement.childNodes[3].textContent;
+x++;
+event.target.parentElement.childNodes[3].textContent = x;
+  if(i%2 == 0){
+  event.target.style.fill = "#ff0000"
+  }
+  else {
+  event.target.style.fill = "forestgreen"
+  }
+  i++;
+}
+
+const addText = function (p) {
+  let territoryName = p.parentElement.id;
+  let t = document.createElementNS("http://www.w3.org/2000/svg", "text");
+  let x = territoriesCoordinates[territoryName].x;
+  let y = territoriesCoordinates[territoryName].y;
+  t.setAttribute("transform", "translate(" + x + " " + y + ")");
+  t.style.fill = "brown";
+  t.style.textAnchor = "middle";
+  t.textContent = p.parentElement.id.replace(/_/g, ' ');
+  t.setAttribute("font-size", "8");
+
+  p.parentNode.insertBefore(t, p.nextSibling);
+  
+  y = y + 12
   let text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-  text.setAttribute("transform", `translate(${xCoordinate} ${yCoordinate})`);
-  text.textContent = "10";
+  text.setAttribute("transform", "translate(" + x + " " + y + ")");
+  text.textContent = '5';
   text.setAttribute("font-size", "8");
   text.style.textAnchor = "middle";
-  path.parentNode.insertBefore(text, path.nextSibling);
+  p.parentNode.insertBefore(text, p.nextSibling);
+  
+  y = y-2.5
   let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-  circle.setAttribute("transform", `translate(${xCoordinate} ${yCoordinate})`);
-  circle.setAttribute("r", 8);
-  circle.style.fill = "grey";
-  path.parentNode.insertBefore(circle, path.nextSibling);
-};
-
+  circle.setAttribute("transform", "translate(" + x + " " + y + ")");
+  circle.setAttribute('r', 8);
+  circle.style.fill = "white";
+  p.parentNode.insertBefore(circle, p.nextSibling);
+}
 let paths = document.querySelectorAll("path");
-paths.forEach(path => {
-  addText(path);
-});
+paths.forEach((x) => { addText(x) }); 
