125561b5278fdae45a55fed8a2e82ab886592b5c
akankshka <44019583+akankshakutal@users.noreply.github.com>
2019-02-18 20:39:15 +0530

    Change pre-commit script

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,22 +1,18 @@
 #! /bin/bash
 
-cat test/*.js |tr "\n" "@"| sed "s/\*\//\*---/g"|sed "s/\/\*/---\/\*/g" |tr "\-\-\-" "\n"|grep "\/\*"| tr "@" "\n" > commentedText.txt
-cat test/*.js | grep '\/\/' >> commentedText.txt
-cat commentedText.txt
-nyc mocha
+nyc mocha --recursive
 nyc check-coverage --functions 100 --branches 100 --lines 100 2> coverageError.txt
 cat coverageError.txt
 coverageErrorCount=$(cat coverageError.txt | wc -l)
-lines=$(cat commentedText.txt|wc -l)
-if [ $lines != 0 -o $coverageErrorCount != 0 ]; then
-  read -p "you have some commented code & coverage is less than threshold \n would you like to commit ? (y/n) : " confirmation
+if [ $coverageErrorCount != 0 ]; then
+  read -p "your coverage is less than threshold \n would you like to commit ? (y/n) : " confirmation
   if [ $confirmation == "y" ]; then
     git commit
   fi
+	rm coverageError.txt
   exit 1;
 fi
+	rm coverageError.txt
 git commit
-rm commentedText.txt
-rm coverageError.txt
 
 
