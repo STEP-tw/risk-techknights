901146caa7278f361c5f986335bf42da4c866ad6
Arif <44020819+azimarif@users.noreply.github.com>
2019-02-20 13:51:49 +0530

    [#7] Prince/Arif - Add test cases for attack handler
    What changes have you made?
    All the test cases for attack handler have been added

--- test/appTest.js
+++ test/appTest.js
@@ -4,11 +4,14 @@ const { Game, Games } = require("../src/models/game");
 const Territory = require("../src/models/territory");
 const Player = require("../src/models/player");
 
-const territory = new Territory("India", ["China"], 10);
+const India = new Territory("India", ["China"], 10);
+const China = new Territory("China", ["India"], 10);
+const Alaska = new Territory("Alaska", ["Alberta"], 10);
+const Alberta = new Territory("Alberta", ["Kamchatka"], 10);
 
 const player = new Player(1, "Player 1", 10);
 const game = new Game(12345, []);
-game.territories = { India: territory };
+game.territories = { India, China, Alaska, Alberta };
 game.addPlayer(player);
 const games = new Games();
 games.addGame(game);
@@ -123,6 +126,7 @@ describe("POST /addPlayer", () => {
     game.addPlayer(player3);
     const player4 = new Player(5, "Player 5", 10);
     game.addPlayer(player4);
+    console.log(game);
     let expected =
       "Oops...  12345 Game is already full. Plase Join any other game";
     game.addPlayer(player3);
@@ -145,22 +149,7 @@ describe("POST /updateWaitingList", () => {
       .end(done);
   });
 
-  it("should respond with 200", done => {
-    const player = new Player(1, "Player 1", 10);
-    const game = new Game(12345, []);
-    game.territories = { India: territory };
-    game.addPlayer(player);
-    const games = new Games();
-    games.addGame(game);
-    app.games = games;
 
-    request(app)
-      .post("/updateWaitingList")
-      .send("gameId=12345")
-      .set("cookie", "game=12345")
-      .expect(200)
-      .end(done);
-  });
 });
 
 describe("/getGamePhase", () => {
@@ -173,22 +162,190 @@ describe("/getGamePhase", () => {
       .expect('{"phase":1}')
       .expect(200, done);
   });
-});
 
-describe("/claimTerritory", () => {
-  it("should respond with 200 ", done => {
-    const countries = ["India", "a", "b", "c"];
-    let territories = {};
-    countries.forEach(country => {
-      territories[country] = new Territory(country, [], 0);
-      territories[country].ruler = true;
-    });
-    game.territories = territories;
+
+
+});
+describe("/attack", () => {
+  it("should set the territoryName as attcking territory ", done => {
     request(app)
-      .post("/claimTerritory")
-      .set("Cookie", "game=12345;playerId=1")
+      .post("/attack")
+      .set("Cookie", "game=12345; playerId=1")
       .send({ territoryName: "India" })
       .expect("Content-Type", /application\/json/)
+      .expect({})
       .expect(200, done);
   });
+
+
+  describe("/attackAgain", () => {
+    it("should attack again with the same territory ", done => {
+      request(app)
+        .post("/attackAgain")
+        .set("Cookie", "game=12345; playerId=1")
+        .send({ territoryName: "India" })
+        .expect("Content-Type", /application\/json/)
+        .expect(200, done);
+    });
+  });
+
+  describe("/battleComplete", () => {
+    it("should finish attack ", done => {
+      request(app)
+        .post("/battleComplete")
+        .set("Cookie", "game=12345; playerId=1")
+        .send({ territoryName: "India" })
+        .expect("Content-Type", /application\/json/)
+        .expect(200, done);
+    });
+  });
+
+
+  describe("/updateCount", () => {
+    it("should update the number of units both the territories lost ", done => {
+      game.attack.defendingTerritory = China;
+      request(app)
+        .post("/updateCount")
+        .send({ attackerLostUnits: 1, defenderLostUnits: 1 })
+        .set("Cookie", "game=12345; playerId=1")
+        .expect("Content-Type", /application\/json/)
+        .expect(200, done);
+    });
+  });
+
+  describe("/attack", () => {
+    it("should  update the number of units both the territories lost", done => {
+      game.attack.attackingTerritory = India;
+      request(app)
+        .post("/attack")
+        .send({ territoryName: 'China' })
+        .send({ attackerLostUnits: 1, defenderLostUnits: 1 })
+        .set("Cookie", "game=12345; playerId=1")
+        .expect("Content-Type", /application\/json/)
+        .expect(200, done);
+    });
+  });
+
+  describe("/updateCount", () => {
+    it("should stop attacking if the militaryUnits becomes zero", done => {
+      China.militaryUnits = 0;
+      game.attack.defendingTerritory = China;
+      request(app)
+        .post("/updateCount")
+        .send({ territoryName: 'China' })
+        .send({ attackerLostUnits: 1, defenderLostUnits: 1 })
+        .set("Cookie", "game=12345; playerId=1")
+        .expect("Content-Type", /application\/json/)
+        .expect(200, done);
+    });
+  });
+
+
+  describe("/attack", () => {
+    it("should allow to attack again from a different territory", done => {
+      game.attack.attackingTerritory = India;
+      request(app)
+        .post("/attack")
+        .send({ territoryName: 'Alaska' })
+        .send({ attackerLostUnits: 1, defenderLostUnits: 1 })
+        .set("Cookie", "game=12345; playerId=1")
+        .expect("Content-Type", /application\/json/)
+        .expect(200, done);
+    });
+  });
+
+  describe("/attack", () => {
+    it("should set defender if attacker is set ", done => {
+      game.attack.attacker = player;
+      request(app)
+        .post("/attack")
+        .send({ territoryName: 'India' })
+        .set("Cookie", "game=12345; playerId=1")
+        .expect("Content-Type", /application\/json/)
+        .expect(200, done);
+    });
+  });
+
+  describe("/attack", () => {
+    it("should not attack if ruler is different ", done => {
+      game.attack.attacker = player;
+      game.attack.attackingTerritory = Alaska;
+      China.ruler = null;
+      request(app)
+        .post("/attack")
+        .send({ territoryName: 'India' })
+        .set("Cookie", "game=12345; playerId=1")
+        .expect("Content-Type", /application\/json/)
+        .expect(200, done);
+    });
+  });
+
+  describe("/attack", () => {
+    it("should not allow attack if militaryUnit is zero ", done => {
+      game.attack.attacker = player;
+      game.attack.attackingTerritory = Alaska;
+      India.militaryUnits = 0;
+      China.ruler = null;
+      request(app)
+        .post("/attack")
+        .send({ territoryName: 'India' })
+        .set("Cookie", "game=12345; playerId=1")
+        .expect("Content-Type", /application\/json/)
+        .expect(200, done);
+    });
+  });
+
+  describe("/attack", () => {
+    it("should not set defendingTerritory if attackingTerritory is not set ", done => {
+      game.attack.attackingTerritory = '';
+      China.ruler = null;
+      request(app)
+        .post("/attack")
+        .send({ territoryName: 'China' })
+        .set("Cookie", "game=12345; playerId=1")
+        .expect("Content-Type", /application\/json/)
+        .expect(200, done);
+    });
+  });
+  describe("/claimTerritory", () => {
+    it("should respond with 200 ", done => {
+      const countries = ["India", "a", "b", "c"];
+      let territories = {};
+      countries.forEach(country => {
+        territories[country] = new Territory(country, [], 0);
+        territories[country].ruler = true;
+      });
+      game.territories = territories;
+      request(app)
+        .post("/claimTerritory")
+        .set("Cookie", "game=12345;playerId=1")
+        .send({ territoryName: "India" })
+        .expect("Content-Type", /application\/json/)
+        .expect(200, done);
+    });
+  });
+
+
 });
+
+
+describe('updateWaitingList', () => {
+
+
+  it("should respond with 200", done => {
+    const player = new Player(1, "Player 1", 10);
+    const game = new Game(12345, []);
+    game.territories = { India };
+    game.addPlayer(player);
+    const games = new Games();
+    games.addGame(game);
+    app.games = games;
+
+    request(app)
+      .post("/updateWaitingList")
+      .send("gameId=12345")
+      .set("cookie", "game=12345")
+      .expect(200)
+      .end(done);
+  });
+})
