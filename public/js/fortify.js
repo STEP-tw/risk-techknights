eec1bb52bf220d8df2622a3091c0537367fcd4f0
Arif <44020819+azimarif@users.noreply.github.com>
2019-02-23 11:51:38 +0530

    Design Place Military Units Section

--- public/js/fortify.js
+++ public/js/fortify.js
@@ -14,24 +14,24 @@ const startFortify = function (event) {
 const displayFortifySection = function (territory) {
   const unit = territory.militaryUnits - 1;
   document.getElementById('selectMilitaryUnit').style.display = 'block';
-  document.getElementById('number').value = unit;
+  document.getElementById('number').innerText = unit;
   document.getElementById('hdnNumber').value = unit;
   document.getElementById('instruction').innerText = 'fortifying...'
 }
 
 const increaseValue = function () {
   const maxValue = parseInt(document.getElementById('hdnNumber').value);
-  let value = parseInt(document.getElementById('number').value);
+  let value = parseInt(document.getElementById('number').innerText);
   value++;
   value > maxValue ? --value : value;
-  document.getElementById('number').value = value;
+  document.getElementById('number').innerText = value;
 }
 
 const decreaseValue = function () {
-  let value = parseInt(document.getElementById('number').value);
+  let value = parseInt(document.getElementById('number').innerText);
   value--;
   value < 0 ? ++value : value;
-  document.getElementById('number').value = value;
+  document.getElementById('number').innerText = value;
 }
 
 const changeToFortifyPhase = function () {
