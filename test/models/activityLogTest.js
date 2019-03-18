b6ccf7f2af37ddb08598f49b6ebecf3dfc387be4
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2019-03-18 17:47:29 +0530

    modified tests of Activity Log.

--- test/models/activityLogTest.js
+++ test/models/activityLogTest.js
@@ -1,35 +1,50 @@
-const { expect } = require('chai');
-const { ActivityLog } = require('../../src/models/activityLog');
+const { expect } = require("chai");
+const { ActivityLog } = require("../../src/models/activityLog");
 let activityLog = new ActivityLog();
 
-describe('getLogs()', function() {
-  it('should return logs with new line', function() {
+describe("getLogs()", function() {
+  it("should return logs with new line", function() {
     activityLog.logId = 1;
-    activityLog.logs[1] = { header: '', events: [] };
-    activityLog.addPlayerLog('player1');
-    expect(activityLog.logs['1']).to.eql({ header: '', events: ['player1'] });
+    activityLog.logs[1] = { header: "", events: [] };
+    activityLog.addPlayerLog("player1");
+    expect(activityLog.logs["1"]).to.eql({ header: "", events: ["player1"] });
   });
 
-  it('should return logs with new line', function() {
+  it("should return logs with new line", function() {
     activityLog.getLogs();
-    expect(activityLog.logs).to.eql({ '1': { header: '', events: ['player1'] } });
-  });
-
-  it('should add a log for attack', function() {
-    attack = {};
-    attack.attackingTerritory = { name: 'India' };
-    attack.defendingTerritory = { name: 'China' };
-    activityLog.attack(attack);
     expect(activityLog.logs).to.eql({
-      '1': { header: '', events: ['player1', 'Attacking from India on China'] }
+      "1": { header: "", events: ["player1"] }
     });
   });
 
-  it('should add a log for reinforceTerritory', function() {
-    territory = { name: 'India' };
+  it("should add a log for attack", function() {
+    attack = {};
+    attack.attackingTerritory = { name: "India" };
+    attack.defendingTerritory = { name: "China" };
+    activityLog.attack(attack);
+    expect(activityLog.logs).to.eql({
+      "1": {
+        header: "",
+        events: [
+          "player1",
+          '<img src="/images/attack.png" style="width:25px;height:25px;"> Attacking from India on China'
+        ]
+      }
+    });
+  });
+
+  it("should add a log for reinforceTerritory", function() {
+    territory = { name: "India" };
     activityLog.reinforceTerritory(territory);
     expect(activityLog.logs).to.eql({
-      '1': { header: '', events: ['player1', 'Attacking from India on China','Reinforced India'] }
+      "1": {
+        header: "",
+        events: [
+          "player1",
+          '<img src="/images/attack.png" style="width:25px;height:25px;"> Attacking from India on China',
+          '<img src="/images/placeMilitary.png" style="width:25px;height:25px;"> Reinforced India'
+        ]
+      }
     });
   });
 });
